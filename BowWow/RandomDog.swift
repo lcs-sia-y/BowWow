6f311c9e7abdcc59bd06e525efe5bb977035dbdb
Sia <zyang@lcs.on.ca>
2020-11-26 13:32:32 -0500

    pulls down a random images of dog

---
+++ BowWow/RandomDog.swift
@@ -0,0 +1,13 @@
+//
+//  RandomDog.swift
+//  BowWow
+//
+//  Created by Zixi Yang on 2020-11-26.
+//
+
+import Foundation
+
+struct RandomDog: Codable {
+    var message: String
+    var status: String
+}
