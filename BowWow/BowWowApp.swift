984ff7e7d1ac18070ba2aff479afb81d1e6e3ec6
Sia <zyang@lcs.on.ca>
2020-11-26 08:56:53 -0500

    Initial Commit

---
+++ BowWow/BowWowApp.swift
@@ -0,0 +1,17 @@
+//
+//  BowWowApp.swift
+//  BowWow
+//
+//  Created by Zixi Yang on 2020-11-26.
+//
+
+import SwiftUI
+
+@main
+struct BowWowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
